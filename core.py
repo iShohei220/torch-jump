variance fix

core.py
@@ -45,15 +45,15 @@ class InferenceCoreGQN(nn.Module):
         super(InferenceCoreGQN, self).__init__()
         self.downsample_x = nn.Conv2d(3, 3, kernel_size=4, stride=4, padding=0, bias=False)
         self.upsample_v = nn.ConvTranspose2d(v_dim, v_dim, kernel_size=16, stride=16, padding=0, bias=False)
-        self.upsample_r = nn.ConvTranspose2d(256, 256, kernel_size=16, stride=16, padding=0, bias=False)
         self.downsample_u = nn.Conv2d(128, 128, kernel_size=4, stride=4, padding=0, bias=False)
         self.core = Conv2dLSTMCell(z_dim+v_dim+256+2*128, 128, kernel_size=5, stride=1, padding=2)
+        self.v_dim = v_dim
         
     def forward(self, x, v, r, c_e, h_e, h_g, u):
-        K = v.size(1)
+        K = v.view(r.size(0), -1, self.v_dim).size(1)
         r = r.view(-1, 1, 256, 16, 16).repeat(1, K, 1, 1, 1).view(-1, 256, 16, 16)
         x = self.downsample_x(x.view(-1, 3, 64, 64))
-        v = self.upsample_v(v.view(-1, v.size(2), 1, 1))
+        v = self.upsample_v(v.view(-1, self.v_dim, 1, 1))
         u = self.downsample_u(u)
         c_e, h_e = self.core(torch.cat((x, v, r, h_g, u), dim=1), (c_e, h_e))
         
@@ -67,12 +67,13 @@ class GenerationCoreGQN(nn.Module):
         self.core = Conv2dLSTMCell(v_dim+256+z_dim, 128, kernel_size=5, stride=1, padding=2)
         self.upsample_h = nn.ConvTranspose2d(128, 128, kernel_size=4, stride=4, padding=0, bias=False)
         self.z_dim = z_dim
+        self.v_dim = v_dim
         
     def forward(self, v, r, c_g, h_g, u, z):
-        K = v.size(1)
+        K = v.view(r.size(0), -1, self.v_dim).size(1)
         r = r.view(-1, 1, 256, 16, 16).repeat(1, K, 1, 1, 1).view(-1, 256, 16, 16)
         z = z.view(-1, self.z_dim, 16, 16)
-        v = self.upsample_v(v.view(-1, v.size(2), 1, 1))
+        v = self.upsample_v(v.view(-1, self.v_dim, 1, 1))
         c_g, h_g =  self.core(torch.cat((v, r, z), dim=1), (c_g, h_g))
         u = self.upsample_h(h_g) + u
         

model.py
@@ -1,8 +1,9 @@
+import numpy as np
 import random
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.distributions import Normal
+from torch.distributions import Normal, Uniform
 from torch.distributions.kl import kl_divergence
 from representation import Pyramid, Tower, Pool
 from core import PriorCore, InferenceCore, GenerationCore, InferenceCoreGQN, GenerationCoreGQN
@@ -40,6 +41,10 @@ class NSG(nn.Module):
     # EstimateELBO
     def forward(self, x, v):
         B, M, *_ = v.size()
+        
+        # Add noise to image
+        noise_range = x.new_ones(x.size()) / (2*255)
+        x = x + Uniform(-noise_range, noise_range).sample()
         
         # Scene encoder
 #         r = torch.sum(self.phi(x.view(-1, 3, 64, 64), v.view(-1, 7)).view(B, -1, 256, 16, 16), dim=1)
@@ -277,6 +282,54 @@ class NSG(nn.Module):
 
         return torch.clamp(mu, 0, 1)
     
+    def inference(self, x, v):
+        B, M, *_ = x.size()
+        
+        # Scene encoder
+#         r = torch.sum(self.phi(x.view(-1, 3, 64, 64), v.view(-1, 7)).view(B, -1, 256, 16, 16), dim=1)
+        r = self.phi(x, v)
+        
+        # Prior initial state
+        c_pi = x.new_zeros((B, 128, 16, 16))
+        h_pi = x.new_zeros((B, 128, 16, 16))
+        z_poe = x.new_zeros((B, self.z_dim, 16, 16))
+        
+        # Inference initial state
+#         c_e = x.new_zeros((B, 128, 16, 16))
+#         h_e = x.new_zeros((B, 128, 16, 16))
+#         z_e = x.new_zeros((B, self.z_dim, 16, 16))
+        c_e = x.new_zeros((B*M, 128, 16, 16))
+        h_e = x.new_zeros((B*M, 128, 16, 16))
+        z_e = x.new_zeros((B*M, self.z_dim, 16, 16))
+        
+        # Prior state update
+        if self.shared_core:
+            c_pi, h_pi = self.prior_core(z_poe, c_pi, h_pi)
+        else:
+            c_pi, h_pi = self.prior_core[l](z_poe, c_pi, h_pi)
+                
+        # Prior factor
+        mu_pi, logvar_pi = torch.split(self.eta_pi(h_pi), self.z_dim, dim=1)
+        std_pi = torch.exp(0.5*logvar_pi)
+        pi = Normal(mu_pi, std_pi)
+            
+        # Inference state update
+        if self.shared_core:
+            c_e, h_e = self.inference_core(z_e, r, c_e, h_e)
+        else:
+            c_e, h_e = self.inference_core[l](z_e, r, c_e, h_e)
+        
+        # Posterior factor
+        mu_q, logvar_q = torch.split(self.eta_e(h_e), self.z_dim, dim=1)
+        std_q = torch.exp(0.5*logvar_q)
+        q = Normal(mu_q.view(B, M, self.z_dim, 16, 16), std_q.view(B, M, self.z_dim, 16, 16))
+        q_poe = self.poe(pi, q)
+            
+        # Posterior sample
+        z_poe = q_poe.sample()
+        
+        return z_poe
+    
     def poe(self, prior, posterior, eps=1e-8):
         vars = torch.cat((prior.variance.view(-1,1,self.z_dim,16,16), posterior.variance), dim=1) + eps
         var = 1. / torch.sum(torch.reciprocal(vars), dim=1) + eps
@@ -285,7 +338,7 @@ class NSG(nn.Module):
         return Normal(loc, torch.sqrt(var))
     
 class CGQN(nn.Module):
-    def __init__(self, L=12, shared_core=True, z_dim=3, v_dim=5, inference_split=True):
+    def __init__(self, L=12, shared_core=True, z_dim=3, v_dim=5):
         super(CGQN, self).__init__()
         
         # Number of generative layers
@@ -293,28 +346,24 @@ class CGQN(nn.Module):
         
         self.z_dim = z_dim
         self.v_dim = v_dim
-        self.inference_split = inference_split
-        
+                
         # Representation network
         self.phi = Tower(v_dim=v_dim)
             
         # Generation network
         self.shared_core = shared_core
         if shared_core:
+            self.prior_core = InferenceCore(z_dim=z_dim)
             self.inference_core = InferenceCore(z_dim=z_dim)
-            if inference_split:
-                self.prior_core = InferenceCore(z_dim=z_dim)
             self.generation_core = GenerationCore(z_dim=z_dim, v_dim=v_dim)
         else:
+            self.prior_core = nn.ModuleList([InferenceCore(z_dim=z_dim) for _ in range(L)])
             self.inference_core = nn.ModuleList([InferenceCore(z_dim=z_dim) for _ in range(L)])
-            if inference_split:
-                self.prior_core = nn.ModuleList([InferenceCore(z_dim=z_dim) for _ in range(L)])
             self.generation_core = nn.ModuleList([GenerationCore(z_dim=z_dim, v_dim=v_dim) for _ in range(L)])
             
+        self.eta_pi = nn.Conv2d(128, 2*z_dim, kernel_size=5, stride=1, padding=2)
         self.eta_e = nn.Conv2d(128, 2*z_dim, kernel_size=5, stride=1, padding=2)
         self.eta_g = nn.Conv2d(128, 3, kernel_size=1, stride=1, padding=0)
-        if inference_split:
-            self.eta_pi = nn.Conv2d(128, 2*z_dim, kernel_size=5, stride=1, padding=2)
         
         self.sigma = Variance([3, 64, 64])
 
@@ -322,15 +371,28 @@ class CGQN(nn.Module):
     def forward(self, x, v, train):
         B, M, *_ = v.size()
         
-        K = random.randint(0, M)
-        context_idx = random.sample(range(M), K)
-        x_c, v_c = x[:, context_idx], v[:, context_idx]
+        # Add noise to image
+        noise_range = x.new_ones(x.size()) / (2*255)
+        x = x + Uniform(-noise_range, noise_range).sample()
         
-        # Scene encoder
-        if len(context_idx)>0 and train==True:
+        if train:
+            K = random.randint(1, M-1)
+            indices = np.random.permutation(range(M))
+            context_idx, target_idx = indices[:K], indices[K:]
+            x_t, v_t = x[:, target_idx], v[:, target_idx]
+            x_c, v_c = x[:, context_idx], v[:, context_idx]
             r = torch.sum(self.phi(x_c, v_c).view(B, -1, 256, 16, 16), dim=1)
         else:
+            x_t, v_t = x, v
             r = x.new_zeros((B, 256, 16, 16))
+            
+        T = x_t.size(1)
+        
+        # Scene encoder
+#         if train:
+#             r = torch.sum(self.phi(x_c, v_c).view(B, -1, 256, 16, 16), dim=1)
+#         else:
+#             r = x.new_zeros((B, 256, 16, 16))
             
         r_T = torch.sum(self.phi(x, v).view(B, -1, 256, 16, 16), dim=1)
 
@@ -344,21 +406,21 @@ class CGQN(nn.Module):
         z_e = x.new_zeros((B, self.z_dim, 16, 16))
         
         # Generator initial state
-        c_g = x.new_zeros((B*M, 128, 16, 16))
-        h_g = x.new_zeros((B*M, 128, 16, 16))
-        u = x.new_zeros((B*M, 128, 64, 64))
+        c_g = x.new_zeros((B*T, 128, 16, 16))
+        h_g = x.new_zeros((B*T, 128, 16, 16))
+        u = x.new_zeros((B*T, 128, 64, 64))
         
         kl = 0
         for l in range(self.L):
             # Prior state update
             if self.shared_core:
-                c_pi, h_pi = self.prior_core(z_e, r, c_pi, h_pi) if self.inference_split else self.inference_core(z_e, r, c_pi, h_pi)
+                c_pi, h_pi = self.prior_core(z_e, r, c_pi, h_pi)
 
             else:
-                c_pi, h_pi = self.prior_core[l](z_e, r, c_pi, h_pi) if self.inference_split else self.inference_core[l](z_e, r, c_pi, h_pi)
+                c_pi, h_pi = self.prior_core[l](z_e, r, c_pi, h_pi)
                 
             # Prior factor
-            mu_pi, logvar_pi = torch.split(self.eta_pi(h_pi) if self.inference_split else self.eta_e(h_pi), self.z_dim, dim=1)
+            mu_pi, logvar_pi = torch.split(self.eta_pi(h_pi), self.z_dim, dim=1)
             std_pi = torch.exp(0.5*logvar_pi)
             pi = Normal(mu_pi, std_pi)
             
@@ -378,15 +440,17 @@ class CGQN(nn.Module):
             
             # Generator state update
             if self.shared_core:
-                c_g, h_g, u = self.generation_core(v, c_g, h_g, u, z_e)
+                c_g, h_g, u = self.generation_core(v_t, c_g, h_g, u, z_e)
             else:
-                c_g, h_g, u = self.generation_core[l](v, c_g, h_g, u, z_e)
+                c_g, h_g, u = self.generation_core[l](v_t, c_g, h_g, u, z_e)
                 
             # ELBO KL contribution update
             kl += torch.sum(kl_divergence(q, pi), dim=[1,2,3])
                 
         # ELBO likelihood contribution update
-        nll = - torch.sum(Normal(self.eta_g(u).view(B, -1, 3, 64, 64), self.sigma()).log_prob(x), dim=[1,2,3,4])
+#         nll = - torch.sum(Normal(self.eta_g(u).view(B, -1, 3, 64, 64), self.sigma()).log_prob(x), dim=[1,2,3,4])
+        nll = - torch.sum(Normal(self.eta_g(u).view(B, -1, 3, 64, 64), self.sigma()).log_prob(x_t), dim=[1,2,3,4])
+
         elbo = nll + kl
 
         return elbo, nll, kl
@@ -409,13 +473,12 @@ class CGQN(nn.Module):
         for l in range(self.L):
             # Prior state update
             if self.shared_core:
-                c_pi, h_pi = self.prior_core(z_pi, r, c_pi, h_pi) if self.inference_split else self.inference_core(z_pi, r, c_pi, h_pi)
-
+                c_pi, h_pi = self.prior_core(z_pi, r, c_pi, h_pi)
             else:
-                c_pi, h_pi = self.prior_core[l](z_pi, r, c_pi, h_pi) if self.inference_split else self.inference_core[l](z_pi, r, c_pi, h_pi)
+                c_pi, h_pi = self.prior_core[l](z_pi, r, c_pi, h_pi)
                 
             # Prior factor
-            mu_pi, logvar_pi = torch.split(self.eta_pi(h_pi) if self.inference_split else self.eta_e(h_pi), self.z_dim, dim=1)
+            mu_pi, logvar_pi = torch.split(self.eta_pi(h_pi), self.z_dim, dim=1)
             std_pi = torch.exp(0.5*logvar_pi)
             pi = Normal(mu_pi, std_pi)
             
@@ -433,17 +496,17 @@ class CGQN(nn.Module):
 
         return torch.clamp(mu, 0, 1)
     
-    def predict(self, x_c, v_c, v_q):
+    def predict(self, x_c, v_c, v_t):
         B, M, *_ = x_c.size()
-        K = v_q.size(1)
+        K = v_t.size(1)
         
         # Scene encoder
         r = torch.sum(self.phi(x_c, v_c).view(B, -1, 256, 16, 16), dim=1)
         
-        # Prior initial state
-        c_pi = x_c.new_zeros((B, 128, 16, 16))
-        h_pi = x_c.new_zeros((B, 128, 16, 16))
-        z_pi = x_c.new_zeros((B, self.z_dim, 16, 16))
+        # Inference initial state
+        c_e = x_c.new_zeros((B, 128, 16, 16))
+        h_e = x_c.new_zeros((B, 128, 16, 16))
+        z_e = x_c.new_zeros((B, self.z_dim, 16, 16))
         
         # Generator initial state
         c_g = x_c.new_zeros((B*K, 128, 16, 16))
@@ -453,24 +516,23 @@ class CGQN(nn.Module):
         for l in range(self.L):
             # Prior state update
             if self.shared_core:
-                c_pi, h_pi = self.prior_core(z_pi, r, c_pi, h_pi) if self.inference_split else self.inference_core(z_pi, r, c_pi, h_pi)
-
+                c_pi, h_pi = self.prior_core(z_e, r, c_e, h_e)
             else:
-                c_pi, h_pi = self.prior_core[l](z_pi, r, c_pi, h_pi) if self.inference_split else self.inference_core[l](z_pi, r, c_pi, h_pi)
-                
-            # Prior factor
-            mu_pi, logvar_pi = torch.split(self.eta_pi(h_pi) if self.inference_split else self.eta_e(h_pi), self.z_dim, dim=1)
-            std_pi = torch.exp(0.5*logvar_pi)
-            pi = Normal(mu_pi, std_pi)
+                c_pi, h_pi = self.prior_core[l](z_e, r, c_e, h_e)
             
-            # Prior sample
-            z_e = pi.rsample()
+            # Prior factor
+            mu_pi, logvar_pi = torch.split(self.eta_pi(h_pi), self.z_dim, dim=1)
+            std_pi = torch.exp(0.5*logvar_pi)
+            p = Normal(mu_pi, std_pi)
+            
+            # Posterior sample
+            z_e = p.rsample()
             
             # Generator state update
             if self.shared_core:
-                c_g, h_g, u = self.generation_core(v_q, c_g, h_g, u, z_e)
+                c_g, h_g, u = self.generation_core(v_t, c_g, h_g, u, z_e)
             else:
-                c_g, h_g, u = self.generation_core[l](v_q, c_g, h_g, u, z_e)
+                c_g, h_g, u = self.generation_core[l](v_t, c_g, h_g, u, z_e)
                 
         # Image sample
         mu = self.eta_g(u).view(B, K, 3, 64, 64)
@@ -548,27 +610,45 @@ class GQN(nn.Module):
         self.sigma = Variance([3, 64, 64])
 
     # EstimateELBO
-    def forward(self, x, v, train):
+    def forward(self, x, v, sigma, train):
         B, M, *_ = v.size()
         
-        K = random.randint(0, M)
-        context_idx = random.sample(range(M), K)
-        x_c, v_c = x[:, context_idx], v[:, context_idx]
+        # Add noise to image
+        noise_range = x.new_ones(x.size()) / (2*255)
+        x = x + Uniform(-noise_range, noise_range).sample()
         
-        # Scene encoder
-        if len(context_idx)>0 and train==True:
+#         K = random.randint(1, M-1)
+#         indices = np.random.permutation(range(M))
+#         context_idx, target_idx = indices[:K], indices[K]
+# #         context_idx = random.sample(range(M), K)
+#         x_c, v_c = x[:, context_idx], v[:, context_idx]
+        if train:
+            K = random.randint(1, M-1)
+            indices = np.random.permutation(range(M))
+            context_idx, target_idx = indices[:K], indices[K]
+            x_t, v_t = x[:, target_idx], v[:, target_idx]
+            T = 1
+            x_c, v_c = x[:, context_idx], v[:, context_idx]
             r = torch.sum(self.phi(x_c, v_c).view(B, -1, 256, 16, 16), dim=1)
         else:
             r = x.new_zeros((B, 256, 16, 16))
-            
+            x_t, v_t = x, v
+            T = M
+        
+#         # Scene encoder
+#         if train:
+#             r = torch.sum(self.phi(x_c, v_c).view(B, -1, 256, 16, 16), dim=1)
+#         else:
+#             r = x.new_zeros((B, 256, 16, 16))
+                        
         # Inference initial state
-        c_e = x.new_zeros((B*M, 128, 16, 16))
-        h_e = x.new_zeros((B*M, 128, 16, 16))
+        c_e = x.new_zeros((B*T, 128, 16, 16))
+        h_e = x.new_zeros((B*T, 128, 16, 16))
                         
         # Generator initial state
-        c_g = x.new_zeros((B*M, 128, 16, 16))
-        h_g = x.new_zeros((B*M, 128, 16, 16))
-        u = x.new_zeros((B*M, 128, 64, 64))
+        c_g = x.new_zeros((B*T, 128, 16, 16))
+        h_g = x.new_zeros((B*T, 128, 16, 16))
+        u = x.new_zeros((B*T, 128, 64, 64))
                 
         kl = 0
         for l in range(self.L):
@@ -579,9 +659,9 @@ class GQN(nn.Module):
             
             # Inference state update
             if self.shared_core:
-                c_e, h_e = self.inference_core(x, v, r, c_e, h_e, h_g, u)
+                c_e, h_e = self.inference_core(x_t, v_t, r, c_e, h_e, h_g, u)
             else:
-                c_e, h_e = self.inference_core[l](x, v, r, c_e, h_e, h_g, u)
+                c_e, h_e = self.inference_core[l](x_t, v_t, r, c_e, h_e, h_g, u)
             
             # Posterior factor
             mu_q, logvar_q = torch.split(self.eta_e(h_e), self.z_dim, dim=1)
@@ -593,15 +673,16 @@ class GQN(nn.Module):
             
             # Generator state update
             if self.shared_core:
-                c_g, h_g, u = self.generation_core(v, r, c_g, h_g, u, z)
+                c_g, h_g, u = self.generation_core(v_t, r, c_g, h_g, u, z)
             else:
-                c_g, h_g, u = self.generation_core[l](v, r, c_g, h_g, u, z)
+                c_g, h_g, u = self.generation_core[l](v_t, r, c_g, h_g, u, z)
                 
             # KL contribution update
             kl += torch.sum(kl_divergence(q, pi), dim=[1,2,3,4])
                 
         # likelihood contribution update
-        nll = - torch.sum(Normal(self.eta_g(u).view(B, -1, 3, 64, 64), self.sigma()).log_prob(x), dim=[1,2,3,4])
+#         nll = - torch.sum(Normal(self.eta_g(u).view(B, -1, 3, 64, 64), self.sigma()).log_prob(x.view(B, -1, 3, 64, 64)), dim=[1,2,3,4])
+        nll = - torch.sum(Normal(self.eta_g(u).view(B, -1, 3, 64, 64), sigma).log_prob(x.view(B, -1, 3, 64, 64)), dim=[1,2,3,4])
         elbo = nll + kl
 
         return elbo, nll, kl

representation.py
@@ -45,7 +45,7 @@ class Tower(nn.Module):
         self.bn8 = nn.BatchNorm2d(256)
 
     def forward(self, x, v):
-        x = x.view(-1, 3, 64, 64)
+        x = x.contiguous().view(-1, 3, 64, 64)
         # Resisual connection
         skip_in  = F.relu(self.bn1(self.conv1(x)))
         skip_out = F.relu(self.bn2(self.conv2(skip_in)))
@@ -54,7 +54,7 @@ class Tower(nn.Module):
         r = F.relu(self.bn4(self.conv4(r))) + skip_out
 
         # Broadcast
-        v = v.view(-1, v.size(2), 1, 1).repeat(1, 1, 16, 16)
+        v = v.contiguous().view(-1, v.size(2), 1, 1).repeat(1, 1, 16, 16)
         
         # Resisual connection
         # Concatenate

train.py
@@ -17,11 +17,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generative Query Network Implementation')
     parser.add_argument('--gradient_steps', type=int, default=2*10**6, help='number of gradient steps to run (default: 2 million)')
     parser.add_argument('--batch_size', type=int, default=36, help='size of batch (default: 36)')
-    parser.add_argument('--dataset', type=str, default='Shepard-Metzler', help='dataset (dafault: Shepard-Metzler)')
+    parser.add_argument('--dataset', type=str, default='Room', help='dataset (dafault: Room)')
     parser.add_argument('--train_data_dir', type=str, help='location of training data', \
-                        default="/workspace/data/GQN/shepard_metzler_7_parts-torch/train")
+                        default="/workspace/dataset/GQN/rooms_free_camera_with_object_rotations-torch/train")
     parser.add_argument('--test_data_dir', type=str, help='location of test data', \
-                        default="/workspace/data/GQN/shepard_metzler_7_parts-torch/test")
+                        default="/workspace/dataset/GQN/rooms_free_camera_with_object_rotations-torch/test")
     parser.add_argument('--root_log_dir', type=str, help='root location of log', default='/workspace/logs')
     parser.add_argument('--log_dir', type=str, help='log directory (default: NSG)', default='NSG')
     parser.add_argument('--log_interval', type=int, help='interval number of steps for logging', default=100)
@@ -29,14 +29,14 @@ if __name__ == '__main__':
     parser.add_argument('--workers', type=int, help='number of data loading workers', default=0)
     parser.add_argument('--device_ids', type=int, nargs='+', help='list of CUDA devices (default: [0])', default=[0])
     parser.add_argument('--representation', type=str, help='representation network (default: tower)', default='tower')
-    parser.add_argument('--layers', type=int, help='number of generative layers (default: 12)', default=12)
+    parser.add_argument('--layers', type=int, help='number of generative layers (default: 6)', default=6)
     parser.add_argument('--shared_core', type=bool, \
                         help='whether to share the weights of the cores across generation steps (default: True)', \
                         default=True)
     parser.add_argument('--z_dim', type=int, default=3)
     parser.add_argument('--v_dim', type=int, default=5)
-    parser.add_argument('--M', type=int, help='M in test', default=3)
-    parser.add_argument('--num_epoch', type=int, help='number of epochs', default=100)
+    parser.add_argument('--M', type=int, help='M in test', default=5)
+    parser.add_argument('--num_epoch', type=int, help='number of epochs', default=20)
     parser.add_argument('--seed', type=int, help='random seed (default: None)', default=None)
     parser.add_argument('--model', type=str, help='which model to use (default: NSG)', default='NSG')
     args = parser.parse_args()
@@ -74,8 +74,8 @@ if __name__ == '__main__':
     D = args.dataset
     
     # Pixel standard-deviation
-#     sigma_i, sigma_f = 2.0, 0.7
-#     sigma = sigma_i
+    sigma_i, sigma_f = 2.0, 0.7
+    sigma = sigma_i
 
     # Number of scenes over which each weight update is computed
     B = args.batch_size
@@ -99,17 +99,26 @@ if __name__ == '__main__':
     if len(args.device_ids)>1:
         model = nn.DataParallel(model, device_ids=args.device_ids)
 
+#     if args.model == "GQN" or args.model == "CGQN":
+    if args.model == "GQN":
+        # Pixel standard-deviation
+        sigma_i, sigma_f = 2.0, 0.7
+        sigma = sigma_i
+        
+        optimizer = torch.optim.Adam(model.parameters(), lr=5e-4, betas=(0.9, 0.999), eps=1e-08)
+        scheduler = AnnealingStepLR(optimizer, mu_i=5e-4, mu_f=5e-5, n=1.6e6)
+    else:
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.999), eps=1e-08)
+
 #     optimizer = torch.optim.Adam(model.parameters(), lr=5e-4, betas=(0.9, 0.999), eps=1e-08)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.999), eps=1e-08)
-
-#     optimizer = torch.optim.Adam(model.parameters())
-
+#     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.999), eps=1e-08)
+    
 #     scheduler = AnnealingStepLR(optimizer, mu_i=5e-4, mu_f=5e-5, n=1.6e6)
-
+    
     kwargs = {'num_workers':num_workers, 'pin_memory': True} if torch.cuda.is_available() else {}
 
     train_loader = DataLoader(train_dataset, batch_size=B, shuffle=True, **kwargs)
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, **kwargs)
+    test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False, **kwargs)
 
 #     train_iter = iter(train_loader)
     x_data_test, v_data_test = next(iter(test_loader))
@@ -117,16 +126,18 @@ if __name__ == '__main__':
     step = 0
     # Training Iterations
     for epoch in range(args.num_epoch):
-#         if len(args.device_ids)>1:
-#             model.module.sigma.param.requires_grad = True if epoch>=args.num_epoch//10 else False
-#         else:
-#             model.sigma.param.requires_grad = True if epoch>=args.num_epoch//10 else False
+        if len(args.device_ids)>1:
+            model.module.sigma.param.requires_grad = True if epoch>=args.num_epoch//2 else False
+        else:
+            model.sigma.param.requires_grad = True if epoch>=args.num_epoch//2 else False
             
         for t, (x_data, v_data) in enumerate(tqdm(train_loader)):
             model.train()
             x, v = sample_batch(x_data, v_data, D)
             x, v = x.to(device), v.to(device)
-            if args.model == "GQN" or args.model == "CGQN":
+            if args.model == "GQN":
+                train_elbo, train_nll, train_kl = model(x, v, sigma, True)
+            elif args.model == "CGQN":
                 train_elbo, train_nll, train_kl = model(x, v, True)
             else:
                 train_elbo, train_nll, train_kl = model(x, v)
@@ -137,6 +148,16 @@ if __name__ == '__main__':
             # Update parameters
             optimizer.step()
             optimizer.zero_grad()
+            
+#             # Pixel-variance annealing
+#             sigma = max(sigma_f + (sigma_i - sigma_f)*(1 - step/(2e5)), sigma_f)
+            
+#             if args.model == "GQN" or args.model == "CGQN":
+            if args.model == "GQN":
+                # Update optimizer state
+                scheduler.step()
+                # Pixel-variance annealing
+                sigma = max(sigma_f + (sigma_i - sigma_f)*(1 - step/(2e5)), sigma_f)
             
             # Update optimizer state
 #             scheduler.step()
@@ -153,14 +174,18 @@ if __name__ == '__main__':
                     x_test, v_test = sample_batch(x_data_test, v_data_test, D, seed=0)
                     x_test, v_test = x_test.to(device), v_test.to(device)
                     
-                    if args.model == "GQN" or args.model == "CGQN":
+                    if args.model == "GQN":
+                        test_elbo, test_nll, test_kl = model(x_test, v_test, sigma, False)
+                    elif args.model == "CGQN":
                         test_elbo, test_nll, test_kl = model(x_test, v_test, False)  
                     else:
                         test_elbo, test_nll, test_kl = model(x_test, v_test)
 
-                    random.seed(0)
-                    context_idx = random.sample(range(x_test.size(1)), M)
-                    x_context, v_context = x_test[:, context_idx], v_test[:, context_idx]
+#                     random.seed(0)
+#                     context_idx = random.sample(range(x_test.size(1)), M)
+#                     x_context, v_context = x_test[:, context_idx], v_test[:, context_idx]
+                    x_context, v_context = x_test[:, :M], v_test[:, :M]
+
                     if len(args.device_ids)>1:
                         x_gen = model.module.generate(v_test)
                         x_pred = model.module.predict(x_context, v_context, v_test)
@@ -173,11 +198,11 @@ if __name__ == '__main__':
                     writer.add_scalar('test_elbo', test_elbo.mean(), step)
                     writer.add_scalar('test_nll', test_nll.mean(), step)
                     writer.add_scalar('test_kl', test_kl.mean(), step)
-                    writer.add_image('test_context', make_grid(x_context[0], 5, pad_value=1), step)
-                    writer.add_image('test_ground_truth', make_grid(x_test[0], 5, pad_value=1), step)
-                    writer.add_image('test_prediction', make_grid(x_pred[0], 5, pad_value=1), step)
-                    writer.add_image('test_generation', make_grid(x_gen[0], 5, pad_value=1), step)
-                    writer.add_image('test_reconstruction', make_grid(x_rec[0], 5, pad_value=1), step)
+                    writer.add_image('test_context', make_grid(x_context.contiguous().view(-1, 3, 64, 64), M, pad_value=1), step)
+                    writer.add_image('test_ground_truth', make_grid(x_test.contiguous().view(-1, 3, 64, 64), 10, pad_value=1), step)
+                    writer.add_image('test_prediction', make_grid(x_pred.contiguous().view(-1, 3, 64, 64), 10, pad_value=1), step)
+                    writer.add_image('test_generation', make_grid(x_gen.contiguous().view(-1, 3, 64, 64), 10, pad_value=1), step)
+                    writer.add_image('test_reconstruction', make_grid(x_rec.contiguous().view(-1, 3, 64, 64), 10, pad_value=1), step)
 
             step += 1
             